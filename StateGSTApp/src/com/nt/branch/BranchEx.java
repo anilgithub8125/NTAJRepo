cd0cafc76819bb0870ef52f5f797b229f971dca1
sri <sri@sri-PC>
2020-05-11 00:31:32 +0530

    new2

--- StateGSTApp/src/com/nt/branch/BranchEx.java
+++ StateGSTApp/src/com/nt/branch/BranchEx.java
@@ -4,5 +4,6 @@ public class BranchEx {
 
 	public static void main(String args[]) {
 		System.out.println("BranchEx main method executed..");
+		System.out.println("BranchEx main method executed..");
 	}
 }
---
+++ StateGSTApp/src/com/nt/servlet1/Example.java
@@ -0,0 +1,8 @@
+package com.nt.servlet1;
+
+public class Example {
+
+	public static void main(String args[]) {
+		System.out.println("skdfjlsad");
+	}
+}
